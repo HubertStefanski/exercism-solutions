94a28ea6a8c7a1079d3373ddaad5b48d2dbd8db9
Hubert Stefanski <35736504+HubertStefanski@users.noreply.github.com>
2022-02-02 20:07:48 +0000

    add hamming solution

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,15 +1,28 @@
 local Hamming = {}
 
-function Hamming.compute(a, b)
-
-end
-
-local function splitString(s, delimiter)
-    result = {}
-    for match in (s .. delimiter):gmatch("" .. delimiter) do
-        table.insert(result, match)
+local function split(s)
+    local result = {}
+    for w in string.gmatch(s, ".") do
+        table.insert(result, w)
     end
     return result
+end
+
+function Hamming.compute(a, b)
+    local a_split = split(a)
+    local b_split = split(b)
+
+    if #a_split == #b_split then
+        local distance = 0
+        for k, v in pairs(a_split) do
+            if b_split[k] ~= v then
+                distance = distance + 1
+            end
+        end
+        return distance
+    else
+        return -1
+    end
 end
 
 return Hamming
