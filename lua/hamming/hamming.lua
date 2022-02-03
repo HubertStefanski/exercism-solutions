c2bfd632c5205eeaccea8bc70d1711e2aac78283
Hubert Stefanski <35736504+HubertStefanski@users.noreply.github.com>
2022-02-03 19:58:26 +0000

    simplify hamming

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,21 +1,10 @@
 local Hamming = {}
 
-local function split(s)
-    local result = {}
-    for w in string.gmatch(s, ".") do
-        table.insert(result, w)
-    end
-    return result
-end
-
 function Hamming.compute(a, b)
-    local a_split = split(a)
-    local b_split = split(b)
-
-    if #a_split == #b_split then
+    if #a == #b then
         local distance = 0
-        for k, v in pairs(a_split) do
-            if b_split[k] ~= v then
+        for i = 0, #a, 1 do
+            if string.sub(a, i, i) ~= string.sub(b, i, i) then
                 distance = distance + 1
             end
         end
@@ -26,3 +15,31 @@ function Hamming.compute(a, b)
 end
 
 return Hamming
+
+-- First attempt VvVvV
+-- local function split(s)
+--     local result = {}
+--     for w in string.gmatch(s, ".") do
+--         table.insert(result, w)
+--     end
+--     return result
+-- end
+
+-- function Hamming.compute(a, b)
+--     local a_split = split(a)
+--     local b_split = split(b)
+
+--     if #a_split == #b_split then
+--         local distance = 0
+--         for k, v in pairs(a_split) do
+--             if b_split[k] ~= v then
+--                 distance = distance + 1
+--             end
+--         end
+--         return distance
+--     else
+--         return -1
+--     end
+-- end
+
+-- return Hamming
