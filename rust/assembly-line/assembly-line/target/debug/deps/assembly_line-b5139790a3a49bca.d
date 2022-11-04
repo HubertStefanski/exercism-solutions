0c58155bd4646884096718cafc2cd0154dadc827
Hubert Stefanski <35736504+HubertStefanski@users.noreply.github.com>
2022-11-04 23:26:55 +0100

    add solution to assembly-line

---
+++ rust/assembly-line/assembly-line/target/debug/deps/assembly_line-b5139790a3a49bca.d
@@ -0,0 +1,5 @@
+/var/home/hstefans/exercism/rust/assembly-line/target/debug/deps/assembly_line-b5139790a3a49bca: tests/assembly-line.rs
+
+/var/home/hstefans/exercism/rust/assembly-line/target/debug/deps/assembly_line-b5139790a3a49bca.d: tests/assembly-line.rs
+
+tests/assembly-line.rs:
